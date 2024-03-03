e6c61537c97879356dc80267636da8d078a4b5cc
Rapuska <mikalajsiatkouski@gmail.com>
2024-03-03 12:37:24 +0300

    Reoganize

--- src/21-Deckbuilder/Assets/Code/View/LifeCycle/Components.cs
+++ src/21-Deckbuilder/Assets/Code/View/LifeCycle/Components.cs
@@ -3,8 +3,6 @@ using Entitas.Generic;
 
 namespace Code.Component
 {
-	public sealed class ShowOnDealEnd : FlagComponent, IInScope<Game> { }
-
 	public sealed class Visible : ValueComponent<bool>, IInScope<Game>, IEvent<Self> { }
 
 	public sealed class Destroyed : FlagComponent, IInScope<Game>, ICleanup<DestroyEntity>, IEvent<Self> { }
