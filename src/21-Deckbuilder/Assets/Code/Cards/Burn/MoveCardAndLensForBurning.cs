54d2b9a5d765f3fbb7bceb57dca69a6661d2a217
Rapuska <mikalajsiatkouski@gmail.com>
2024-03-04 21:42:01 +0300

    Fix stuff movement

--- src/21-Deckbuilder/Assets/Code/Cards/Burn/MoveCardAndLensForBurning.cs
+++ src/21-Deckbuilder/Assets/Code/Cards/Burn/MoveCardAndLensForBurning.cs
@@ -1,42 +1,44 @@
+using System.Collections.Generic;
 using Code.Component;
 using Code.Scope;
 using Entitas;
 using Entitas.Generic;
 using UnityEngine;
-using Zenject;
+using static Entitas.Generic.ScopeMatcher<Code.Scope.Game>;
 
 namespace Code.System
 {
-	public sealed class MoveCardAndLensForBurning : IExecuteSystem
+	public sealed class MoveCardAndLensForBurning : ReactiveSystem<Entity<Game>>
 	{
 		private readonly HoldersProvider _holders;
-		private readonly IGroup<Entity<Game>> _cards;
 		private readonly IGroup<Entity<Game>> _lenses;
 
-		[Inject]
-		public MoveCardAndLensForBurning(Contexts contexts, HoldersProvider holdersProvider)
+		public MoveCardAndLensForBurning(Contexts contexts, HoldersProvider holders)
+			: base(contexts.Get<Game>())
 		{
-			_holders = holdersProvider;
-			_cards = contexts.GetGroup(ScopeMatcher<Game>.Get<ToBurn>());
-			_lenses = contexts.GetGroup(ScopeMatcher<Game>.Get<Lens>());
+			_holders = holders;
+			_lenses = contexts.GetGroup(Get<Lens>());
 		}
 
-		public void Execute()
-		{
-			foreach (var candidate in _cards)
-			{
-				var target = _holders.BurnCard;
+		protected override ICollector<Entity<Game>> GetTrigger(IContext<Entity<Game>> context)
+			=> context.CreateCollector(AllOf(Get<Card>(), Get<ToBurn>()).AddedOrRemoved());
 
-				candidate.Replace<TargetPosition, Vector3>(target.position);
-				candidate.Replace<TargetRotation, Quaternion>(target.rotation);
+		protected override bool Filter(Entity<Game> entity) => true;
+
+		protected override void Execute(List<Entity<Game>> entities)
+		{
+			foreach (var card in entities)
+			{
+				var cardTarget = _holders.BurnCard;
+				card.Replace<TargetPosition, Vector3>(cardTarget.position);
+				card.Replace<TargetRotation, Quaternion>(cardTarget.rotation);
 			}
 
 			foreach (var lens in _lenses)
 			{
-				var target = _holders.BurnLoupe;
-
-				lens.Replace<TargetPosition, Vector3>(target.position);
-				lens.Replace<TargetRotation, Quaternion>(target.rotation);
+				var lensTarget = _holders.BurnLoupe;
+				lens.Replace<TargetPosition, Vector3>(lensTarget.position);
+				lens.Replace<TargetRotation, Quaternion>(lensTarget.rotation);
 			}
 		}
 	}
