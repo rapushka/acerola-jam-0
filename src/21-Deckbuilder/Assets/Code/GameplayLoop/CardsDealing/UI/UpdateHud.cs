7dd84eaaf9e4cf958e7833a67336f917dfc913b6
Rapuska <mikalajsiatkouski@gmail.com>
2024-03-06 22:58:55 +0300

    Even better hud!

--- src/21-Deckbuilder/Assets/Code/GameplayLoop/CardsDealing/UI/UpdateHud.cs
+++ src/21-Deckbuilder/Assets/Code/GameplayLoop/CardsDealing/UI/UpdateHud.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Code.Component;
 using Code.Scope;
 using Code.System;
@@ -11,19 +12,22 @@ namespace Code
 	{
 		private readonly Contexts _contexts;
 		private readonly HudMediator _hud;
+		private readonly IGroup<Entity<Game>> _timers;
 
 		[Inject]
 		public UpdateHud(Contexts contexts, HudMediator hud)
 		{
 			_contexts = contexts;
 			_hud = hud;
+			_timers = contexts.GetGroup(ScopeMatcher<Game>.Get<Waiting>());
 		}
 
 		public void Execute()
 		{
 			var isOurTurn = _contexts.GetPlayer().Is<CurrentTurn>();
+			var waitingForSomething = _timers.GetEntities().Any();
 
-			if (!isOurTurn)
+			if (!isOurTurn || waitingForSomething)
 			{
 				_hud.TurnActionsVisibility = false;
 				_hud.PickCardOptionsVisibility = false;
