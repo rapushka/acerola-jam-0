e826afdc6ae3b18d3bc6b8532702935f63bd9380
Rapuska <mikalajsiatkouski@gmail.com>
2024-03-10 18:09:28 +0300

    Fix end scoring

--- src/21-Deckbuilder/Assets/Code/Players/Scores/ScoringState/EndScoring.cs
+++ src/21-Deckbuilder/Assets/Code/Players/Scores/ScoringState/EndScoring.cs
@@ -1,39 +1,43 @@
 using System;
+using System.Collections.Generic;
 using Code.Component;
 using Code.Scope;
 using Entitas;
 using Entitas.Generic;
+using UnityEngine;
 
 namespace Code.Players
 {
-	public sealed class EndScoring : IExecuteSystem
+	public sealed class EndScoring : ReactiveSystem<Entity<Game>>
 	{
 		private readonly Contexts _contexts;
 		private readonly ViewConfig _viewConfig;
 		private readonly IGroup<Entity<Game>> _entities;
 
-		public EndScoring(Contexts contexts, ViewConfig viewConfig)
+		public EndScoring(Contexts contexts, ViewConfig viewConfig) : base(contexts.Get<Game>())
 		{
 			_contexts = contexts;
 			_viewConfig = viewConfig;
-			_entities = contexts.GetGroup(ScopeMatcher<Game>.Get<Scoring>());
 		}
 
-		public void Execute()
-		{
-			foreach (var _ in _entities)
-			{
-				var e = _contexts.Get<Game>().CreateEntity();
-				e.Add<Waiting, float>(_viewConfig.ScoringDuration);
-				e.Add<Callback, Action>(End);
-				continue;
+		protected override ICollector<Entity<Game>> GetTrigger(IContext<Entity<Game>> context)
+			=> context.CreateCollector(ScopeMatcher<Game>.Get<Scoring>().Added());
 
-				void End()
-				{
-					_contexts.Get<Game>().CreateEntity().Is<EndDeal>(true);
-					_contexts.StopScoring();
-					e.Is<Destroyed>(true);
-				}
+		protected override bool Filter(Entity<Game> entity) => entity.Is<Scoring>();
+
+		protected override void Execute(List<Entity<Game>> entities)
+		{
+			var e = _contexts.Get<Game>().CreateEntity();
+			e.Add<Waiting, float>(_viewConfig.ScoringDuration);
+			e.Add<Callback, Action>(End);
+			return;
+
+			void End()
+			{
+				Debug.Log("end deal");
+				_contexts.Get<Game>().CreateEntity().Is<EndDeal>(true);
+				_contexts.StopScoring();
+				e.Is<Destroyed>(true);
 			}
 		}
 	}
