95cd131e4544dc16f91caaf65bbb1591679088ea
Rapuska <mikalajsiatkouski@gmail.com>
2024-03-10 22:25:00 +0300

    Slower Lamp rotation

--- src/21-Deckbuilder/Assets/Code/Lamp/Components.cs
+++ src/21-Deckbuilder/Assets/Code/Lamp/Components.cs
@@ -4,4 +4,6 @@ using Entitas.Generic;
 namespace Code.Component
 {
 	public sealed class Lamp : FlagComponent, IInScope<Game>, IUnique { }
+
+	public sealed class RotationSpeed : ValueComponent<float>, IInScope<Game> { }
 }
--- src/21-Deckbuilder/Assets/Code/Lamp/LampBehaviour.cs
+++ src/21-Deckbuilder/Assets/Code/Lamp/LampBehaviour.cs
@@ -2,16 +2,21 @@ using Code.Component;
 using Code.Scope;
 using Entitas.Generic;
 using UnityEngine;
+using Zenject;
 
 namespace Code
 {
 	public class LampBehaviour : ComponentBehaviourBase<Game>
 	{
+		[Inject] private readonly HoldersProvider _holders;
+		[Inject] private readonly ViewConfig _viewConfig;
+
 		public override void Add(ref Entity<Game> entity)
 		{
 			entity.Is<Lamp>(true);
 			entity.Add<DebugName, string>("lamp");
-			entity.Add<Rotation, Quaternion>(transform.rotation);
+			entity.Add<Rotation, Quaternion>(_holders.Lamp.Default.rotation);
+			entity.Add<RotationSpeed, float>(_viewConfig.LampRotationSpeed);
 		}
 	}
 }
