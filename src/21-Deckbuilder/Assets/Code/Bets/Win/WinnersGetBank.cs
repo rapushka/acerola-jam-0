333f3b87b7c6b55a6dac08b6d28e5f1954146022
Rapuska <mikalajsiatkouski@gmail.com>
2024-03-12 00:19:52 +0300

    Send Bet from bank

--- src/21-Deckbuilder/Assets/Code/Bets/Win/WinnersGetBank.cs
+++ src/21-Deckbuilder/Assets/Code/Bets/Win/WinnersGetBank.cs
@@ -9,12 +9,14 @@ namespace Code
 	public sealed class WinnersGetBank : IExecuteSystem
 	{
 		private readonly Contexts _contexts;
+		private readonly BetsFactory _factory;
 		private readonly IGroup<Entity<Game>> _endDeal;
 		private readonly IGroup<Entity<Game>> _winners;
 
-		public WinnersGetBank(Contexts contexts)
+		public WinnersGetBank(Contexts contexts, BetsFactory factory)
 		{
 			_contexts = contexts;
+			_factory = factory;
 			_endDeal = contexts.GetGroup(Get<EndDeal>());
 			_winners = contexts.GetGroup(Get<Winner>());
 		}
@@ -33,7 +35,10 @@ namespace Code
 					var sidePrize = bankMoney / winnersCount;
 
 					foreach (var winner in _winners)
+					{
 						winner.AddMoneyValue(sidePrize);
+						_factory.SendBet(Bank, winner.GetMoneyHeap());
+					}
 				}
 
 				Bank.Replace<Money, int>(0);
